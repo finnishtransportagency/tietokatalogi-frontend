7b963f92b35268c64fe4513ba30da9f5f916660a
Alvar Wihuri <alvar.wihuri@cgi.com>
2022-08-23 18:12:27 +0300

    Frontend UI work

--- src/views/EditableMainPageView.js
+++ src/views/EditableMainPageView.js
@@ -6,15 +6,45 @@ import * as DOMPurify from 'dompurify';
 
 export const EditableMainPageView = () => {
     const [markdown, setMarkdown] = React.useState("");
+    const [edit, setEdit] = React.useState(false);
     return (
         <React.Fragment>
-            <textarea
-                className="tk-field form-control" rows="15"
-                onChange={(e) => setMarkdown(e.target.value)}
-            />
-            <div 
-                dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(marked.parse(markdown))}}
-            />
+            {edit ? 
+                <div>
+                    <textarea
+                        defaultValue={markdown}
+                        className="tk-field form-control" rows="15"
+                        onChange={(e) => setMarkdown(e.target.value)}
+                    />
+                    <div className="btn-toolbar">
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={() => setEdit(false)} // TODO: save should happen here
+                        >
+                            Tallenna
+                        </button>
+                    </div>
+                </div>
+            : 
+            <React.Fragment>
+                <div
+                    style={{ backgroundColor: 'ghostwhite' }}
+                    dangerouslySetInnerHTML={{
+                        __html: DOMPurify.sanitize(marked.parse(markdown))
+                    }} />
+                <div className="btn-toolbar">
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => setEdit(true)}
+                    >
+                        Muokkaa
+                    </button>
+                </div>
+            </React.Fragment>
+            }
+            
         </React.Fragment>
     );
 }
