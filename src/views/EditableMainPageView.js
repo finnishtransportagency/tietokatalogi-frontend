c1ebe82fde4cfad47d13833e8d587521a5032d11
Alvar Wihuri <alvar.wihuri@cgi.com>
2022-08-25 10:23:34 +0300

    Add editor component

--- src/views/EditableMainPageView.js
+++ src/views/EditableMainPageView.js
@@ -2,37 +2,53 @@ import React from 'react'
 
 import * as marked from 'marked';
 import * as DOMPurify from 'dompurify';
+import EasyMDE from 'easymde';
+import "easymde/dist/easymde.min.css";
 
 
 export const EditableMainPageView = () => {
     const [markdown, setMarkdown] = React.useState("");
     const [edit, setEdit] = React.useState(false);
+    const textareaRef = React.useRef(null);
+    const [easyMDE, setEasyMDE] = React.useState(null);
+
+    React.useEffect(() => {
+        const createdEasyMDE = new EasyMDE({
+            element: textareaRef && textareaRef.current,
+            spellChecker: false,
+        });
+        setEasyMDE(createdEasyMDE);
+
+        createdEasyMDE.codemirror.on("change", (e) => {
+            setMarkdown(createdEasyMDE.value());
+        })
+    }, []);
+
     return (
         <React.Fragment>
+            <div style={{ display: edit ? 'inline' : 'none' }}>
+            <textarea
+                ref={textareaRef}
+            />
+            </div>
             {edit ? 
-                <div>
-                    <textarea
-                        defaultValue={markdown}
-                        className="tk-field form-control" rows="15"
-                        onChange={(e) => setMarkdown(e.target.value)}
-                    />
-                    <div className="btn-toolbar">
-                        <button
-                            type="button"
-                            className="btn btn-primary"
-                            onClick={() => setEdit(false)} // TODO: save should happen here
-                        >
-                            Tallenna
-                        </button>
-                    </div>
+                <div className="btn-toolbar">
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => setEdit(false)} // TODO: save should happen here
+                    >
+                        Tallenna
+                    </button>
                 </div>
-            : 
+            :
             <React.Fragment>
                 <div
                     style={{ backgroundColor: 'ghostwhite' }}
                     dangerouslySetInnerHTML={{
-                        __html: DOMPurify.sanitize(marked.parse(markdown))
-                    }} />
+                        __html: DOMPurify.sanitize(marked.parse(markdown)) || "(ei sisältöä)"
+                    }}>
+                </div>
                 <div className="btn-toolbar">
                     <button
                         type="button"
