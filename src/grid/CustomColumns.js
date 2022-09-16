e924bce1b2845cf9d6f92923f9402de9bf610258
Alvar Wihuri <alvar.wihuri@cgi.com>
2022-09-16 13:42:33 +0300

    Add linkified column to toimintaprosessi list

--- src/grid/CustomColumns.js
+++ src/grid/CustomColumns.js
@@ -1,31 +1,47 @@
 import React from "react";
 import { Link } from "react-router";
+import Linkify from "react-linkify";
 import { connect } from "react-redux";
 import { fullURL } from "../App";
 
 // eslint-disable-next-line react/display-name
-const CustomLink = (linkKey) => ({value, griddleKey, rowData}) => (
-    <Link to={ fullURL( (rowData.itemUrl || ""), rowData.tunnus )} className='name-link'>{rowData[linkKey] || griddleKey}</Link>
-);
+const CustomLink =
+  (linkKey) =>
+  ({ value, griddleKey, rowData }) =>
+    (
+      <Link
+        to={fullURL(rowData.itemUrl || "", rowData.tunnus)}
+        className="name-link"
+      >
+        {rowData[linkKey] || griddleKey}
+      </Link>
+    );
 
-const CustomStatus = ({value, griddleKey, rowData}) => (
-    <div>{rowData.aktiivinen === 1 ? "Aktiivinen" : "Ei-aktiivinen"}</div>
+const CustomStatus = ({ value, griddleKey, rowData }) => (
+  <div>{rowData.aktiivinen === 1 ? "Aktiivinen" : "Ei-aktiivinen"}</div>
 );
 
 const rowDataSelector = (state, { griddleKey }) => {
-    return state
-        .get("data")
-        .find(r => r.get("griddleKey") === griddleKey)
-        .toJSON();
+  return state
+    .get("data")
+    .find((r) => r.get("griddleKey") === griddleKey)
+    .toJSON();
 };
 
 const EnhanceWithRowData = connect((state, props) => ({
-    rowData: rowDataSelector(state, props)
+  rowData: rowDataSelector(state, props),
 }));
 
-export const LinkToItemWithLinkKey = (linkKey) => EnhanceWithRowData(CustomLink(linkKey));
+export const LinkToItemWithLinkKey = (linkKey) =>
+  EnhanceWithRowData(CustomLink(linkKey));
 
 export const LinkToItem = EnhanceWithRowData(CustomLink("nimi"));
 // Termilomake uses ensisijainen termi instead of nimi
 export const LinkToItemTermilomake = EnhanceWithRowData(CustomLink("nimi"));
 export const ActiveStatus = EnhanceWithRowData(CustomStatus);
+
+export const LinkifiedItem = ({ value }) => (
+  <Linkify properties={{ target: "_blank", rel: "noopener noreferrer" }}>
+    {value}
+  </Linkify>
+);
--- src/toimintaprosessi/ToimintaprosessiGriddle.js
+++ src/toimintaprosessi/ToimintaprosessiGriddle.js
@@ -2,30 +2,35 @@ import React from "react";
 import CustomGriddle from "../grid/CustomGriddle";
 import { RowDefinition, ColumnDefinition } from "griddle-react";
 import { ToimintaprosessiFilter } from "./ToimintaprosessiFilter";
-import { LinkToItem } from "../grid/CustomColumns";
+import { LinkifiedItem, LinkToItem } from "../grid/CustomColumns";
 
 export default class ToimintaprosessiGriddle extends React.Component {
-    render() {
-        const { dataStore } = this.props;
+  render() {
+    const { dataStore } = this.props;
 
-        return (
-            <CustomGriddle
-                dataStore={dataStore}
-                customfilter={ToimintaprosessiFilter}
-            >
-                <RowDefinition>
-                    <ColumnDefinition
-                        id="nimi"
-                        width={"20%"}
-                        customComponent={LinkToItem}
-                        customHeadingComponent={() => null}
-                    />
-                    <ColumnDefinition
-                        id="vastaava_organisaatio"
-                        customHeadingComponent={() => null}
-                    />
-                </RowDefinition>
-            </CustomGriddle>
-        );
-    }
+    return (
+      <CustomGriddle
+        dataStore={dataStore}
+        customfilter={ToimintaprosessiFilter}
+      >
+        <RowDefinition>
+          <ColumnDefinition
+            id="nimi"
+            width={"20%"}
+            customComponent={LinkToItem}
+            customHeadingComponent={() => null}
+          />
+          <ColumnDefinition
+            id="vastaava_organisaatio"
+            customHeadingComponent={() => null}
+          />
+          <ColumnDefinition
+            id="tyotila"
+            customComponent={({ value }) => <LinkifiedItem value={value} />}
+            customHeadingComponent={() => null}
+          />
+        </RowDefinition>
+      </CustomGriddle>
+    );
+  }
 }
