639c6b35ac014966d204297ad3d30d8f1ac48560
Cedric Neergaard <75533906+CeduDev@users.noreply.github.com>
2023-10-17 11:14:45 +0300

    Analpa 2835 yhteystiedot kannasta (#5)
    * moved axios functions to separate file
    
    * fetched contact info from database
    
    * fixed typo, className, and unnecessary boolean check
    
    * fixed text search

--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -12,7 +12,10 @@ const ContactsView = () => {
             setLoading(false);
             if (!data) return;
             const {sideText = "" } = data;
-            const relevantText = sideText.split("#").find((s) => s.includes("Yhteystiedot"))
+            // Regex explanation: find a line of text only containing the word "Yhteystiedot", with possibly some non-alphanumeric or white-space characters in the beginning or the end
+            // I.e., if the word "Yhteystiedot" occurs in the middle of a sentence, it is not found by the regex
+            // Reason for this pattern is due to how our text is stored in the database
+            const relevantText = sideText.split(/^[^a-zA-Z\d\s:]*Yhteystiedot[^a-zA-Z\d\s:]*$/mg)[1]
             if(!relevantText) setSideText('Tietoa ei löytynyt, tarkista että etusivussa löytyy sivutiedosta tieto muodossa "### Yhteystiedot"')
             else setSideText(relevantText);
         });
