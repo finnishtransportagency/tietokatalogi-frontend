faff2f0b347029d4d6c1a8d91fe69886d3cfb78e
CeduDev <cedric.neergaard@gmail.com>
2023-10-17 14:18:52 +0300

    fixed regex

--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -15,7 +15,7 @@ const ContactsView = () => {
             // Regex explanation: find a line of text only containing the word "Yhteystiedot", with possibly some non-alphanumeric or white-space characters in the beginning or the end
             // I.e., if the word "Yhteystiedot" occurs in the middle of a sentence, it is not found by the regex
             // Reason for this pattern is due to how our text is stored in the database
-            const relevantText = sideText.split(/^[^a-zA-Z\d\s:]*Yhteystiedot[^a-zA-Z\d\s:]*$/mg)[1]
+            const relevantText = sideText.split(/^[^a-zA-Z\d:]*Yhteystiedot[^a-zA-Z\d:]*$/mg)[1]
             if(!relevantText) setSideText('Tietoa ei löytynyt, tarkista että etusivussa löytyy sivutiedosta tieto muodossa "### Yhteystiedot"')
             else setSideText(relevantText);
         });
